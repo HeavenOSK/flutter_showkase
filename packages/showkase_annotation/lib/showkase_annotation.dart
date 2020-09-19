46b7cf0c41e02a47c20d19ad95b81e4d13d177ac
heavenOSK <ryunosukeheaven.ba@gmail.com>
2020-09-19 23:47:38 +0900

    add: add annotations for showkase

--- packages/showkase_annotation/lib/showkase_annotation.dart
+++ packages/showkase_annotation/lib/showkase_annotation.dart
@@ -1,7 +1,32 @@
 library showkase_annotation;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+/// A type of [ShowkaseTheme].
+enum ShowkaseThemeType {
+  light,
+  dark,
+}
+
+/// An annotation for the `showkase` package.
+///
+/// Annotating a function with this annotation will flag it to add
+/// `showkase` app.
+///
+/// This annotation expects only function which returns [ThemeData].
+class ShowkaseTheme {
+  const ShowkaseTheme.light() : type = ShowkaseThemeType.light;
+  const ShowkaseTheme.dark() : type = ShowkaseThemeType.dark;
+
+  final ShowkaseThemeType type;
+}
+
+/// An annotation for the `showkase` package.
+///
+/// Annotating a function with this annotation will flag it to add
+/// `showkase` app.
+///
+/// This annotation expects only function which returns [Widget].
+class ShowkaseComponent {
+  const ShowkaseComponent(this.name);
+
+  final String name;
 }
